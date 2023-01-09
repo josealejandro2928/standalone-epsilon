a48fecc5c1c502eccbaae3caba4cc9f93124924c
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2023-01-09 17:49:04 +0100

    adding runtime config

--- src/main/java/org/standalone/epsilon/example/Main.java
+++ src/main/java/org/standalone/epsilon/example/Main.java
@@ -8,12 +8,13 @@ public class Main {
         String modelOnePath = Paths.get("scripts", "eol", "example_models", "SecuritySystem_Impl.aaxl2").toAbsolutePath().toString();
         String modelTwoPath = Paths.get("scripts", "eol", "example_models", "UAS_Impl.aaxl2").toAbsolutePath().toString();
         try {
-            System.out.println("---------------Running the script for the model: " + modelOnePath +" ----------------------------");
+            System.out.println("---------------Running the script for the model: " + modelOnePath + " ----------------------------");
             Object res1 = eolRunner.run(modelOnePath);
             System.out.println("Output 1: " + res1);
-            System.out.println("\n---------------Running the script for the model: " + modelTwoPath +" ----------------------------");
+
+            System.out.println("\n---------------Running the script for the model: " + modelTwoPath + " ----------------------------");
             Object res2 = eolRunner.run(modelTwoPath);
-            System.out.println("Output 2: " + res1);
+            System.out.println("Output 2: " + res2);
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
